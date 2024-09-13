98d42b823edbfe9020bf9d45675e4024671d1283
Umesh-roka <umeshmagar764@gmail.com>
2024-09-13 15:32:05 +0545

    change

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,8 +1,8 @@
 // export const baseUrl = 'http://192.168.56.1:5000/api';
 // export const imageUrl ='http://192.168.56.1:5000';
 
-export const baseUrl = 'https://powstore-backend.onrender.com/api';
-export const imageUrl ='https://powstore-backend.onrender.com';
+export const baseUrl = 'https://pawstore-backend.onrender.com/api';
+export const imageUrl ='https://pawstore-backend.onrender.com';
 export const userUrl = `${baseUrl}/users`
 export const petUrl = `${baseUrl}/pets`
 export const productUrl = `${baseUrl}/products`
