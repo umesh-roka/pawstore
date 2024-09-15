6a5f81370176d256e18defc18121dda13e34b93d
Umesh-roka <umeshmagar764@gmail.com>
2024-09-16 00:01:09 +0545

    change

--- src/ui/NotGettingData.js
+++ src/ui/NotGettingData.js
@@ -10,7 +10,7 @@ const NotGettingData = () => {
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Not Getting Data</h1>
           <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the data you’re looking for.</p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <NavLink to='/'
+            <NavLink to=''
               className="rounded-md bg-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               refresh
