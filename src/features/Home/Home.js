d1bf0215bb3cd81f3ca8a77ea5af6bfa58355727
Umesh-roka <umeshmagar764@gmail.com>
2024-09-19 14:31:19 +0545

    change

--- src/features/Home/Home.js
+++ src/features/Home/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
   const {user}= useSelector((state)=>state.userSlice);
   return (
     <>
-    <div class="grid sm:grid-cols-1 lg:grid-cols-2 justify-center lg:px-150px] sm:py-[20px]  items-center sm:pt-10 bg-orange-100 gap-4">
+    <div class="grid sm:grid-cols-1 lg:grid-cols-2 justify-center lg:px-200px] sm:py-[20px]  items-center sm:pt-10 bg-orange-100 gap-4">
   <div class=" lg:mt-20 lg:mb-20 lg:mr-20 sm:px-[110px] "><Caro/> </div>
   <div class="... sm:p-3 text-justify"><h1 className='font-bold sm:text-2xl lg:text-3xl mb-4'>EveryBody Needs A Friend In Life</h1>
   <p className=''>Welcome to PawStore, your trusted source for finding the perfect canine or feline companion. Specializing exclusively in dogs and cats, we offer a variety of healthy, well-cared-for breeds along with a wide range of pet accessories, including premium food, grooming supplies, toys, and stylish apparel. Our mission is to help you build a strong bond with your new pet by providing expert advice and high-quality products. Whether you’re looking for a playful pup, a cuddly kitten, or the best accessories to pamper them, PawStore is here to support you every step of the way.</p>
