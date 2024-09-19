ab46e5c29ea2840db6a28c29b84313b74f2d0bbb
Umesh-roka <umeshmagar764@gmail.com>
2024-09-19 14:16:24 +0545

    chnage

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,16 +16,7 @@ module.exports = withMT({
         },
       },
 
-      screens: {
-        'sm': '640px',
-        // => @media (min-width: 640px) { ... }
-  
-        'md': '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-        'lg': '1280px',
-        // => @media (min-width: 1280px) { ... }
-      },
+    
   },
   plugins: [],
 });
