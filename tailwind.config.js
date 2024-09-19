c92d6d3c3cef701d45d00c856ceccf577a100afe
Umesh-roka <umeshmagar764@gmail.com>
2024-09-19 13:28:52 +0545

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,10 +20,10 @@ module.exports = withMT({
         'sm': {'min': '375px', 'max': '767px'},
         // => @media (min-width: 640px and max-width: 767px) { ... }
   
-        'md': {'min': '768px', 'max': '1023px'},
+        // 'md': {'min': '768px', 'max': '1023px'},
         // => @media (min-width: 768px and max-width: 1023px) { ... }
   
-        'lg': {'min': '768px', 'max': ''},
+        'lg': {'min': '1000px', 'max': ''},
         // => @media (min-width: 1024px and max-width: 1279px) { ... }
   
         // 'xl': {'min': '1280px', 'max': '1535px'},
